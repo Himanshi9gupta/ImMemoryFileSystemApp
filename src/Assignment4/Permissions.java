f2039e1b36aa5e6e81fe5f75b9d63bd641e7504e
Himanshi Gupta <98001508+Himanshi9gupta@users.noreply.github.com>
2023-08-29 01:08:09 -0700

    Update Permissions.java

--- src/Assignment4/Permissions.java
+++ src/Assignment4/Permissions.java
@@ -8,40 +8,50 @@ public abstract class Permissions{
 	boolean hasPermission;
 	public static final String admin = "ADMIN";
 	public static final String regular = "REGULAR";
-	static HashMap<String, String> userPermission = new HashMap<>(); 
-	
-	Permissions (String user, String role) { 
+		static HashMap<String, String> userPermission = new HashMap<>();
+
+	Permissions(String user, String role) {
 		this.user = user;
 		this.role = role;
-		if(!userPermission.containsKey(user)) {
+		if (!userPermission.containsKey(user)) {
 			userPermission.put(user, role.toUpperCase());
 		}
-	} 
-	
+	}
+
 	public static HashMap<String, String> setPermissions(String user, String role) {
-		if(!userPermission.containsKey(user) && role.equalsIgnoreCase(admin)) {
+		if (!userPermission.containsKey(user) && role.equalsIgnoreCase(admin)) {
 			userPermission.put(user, role.toUpperCase());
-		}
-		else {
+		} else {
 			userPermission.replace(user, role.toUpperCase());
 		}
 		System.out.println("userpermission" + userPermission);
 		return userPermission;
 	}
-	
-	public static boolean hasPermissions(String user) { 
-		String role = userPermission.get(user);
-		if(userPermission.containsKey(user) && role.equalsIgnoreCase(admin)){ 
-			return true; 
-		} 
-		return false;	
-	}
-	
-	public static void revokePermissions(String user){ 
-		if(hasPermissions(user)){ 
-			userPermission.replace(user, regular); 
-		} 
 
-	} 
+	public static boolean hasPermissions(String user, Connection connection) {
+		String sql = "SELECT * FROM users WHERE firstName = '" + user + "'";
+		try {
+
+			Statement stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
+			//stmt.close();
+			System.out.println(rs);
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+
+		String role = userPermission.get(user);
+		if (userPermission.containsKey(user) && role.equalsIgnoreCase(admin)) {
+			return true;
+		}
+		return false;
+	}
+
+	public static void revokePermissions(String user, Connection connection) {
+		if (hasPermissions(user, connection)) {
+			userPermission.replace(user, regular);
+		}
+
+	}
 
 }
